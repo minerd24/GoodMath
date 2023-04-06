c360cf078d940ffa747ac96308b19c8e57c30c84
Evan Wall <minerd24@gmail.com>
2023-04-06 16:00:28 -0400

    Fixed Bugs. All good now.

--- BadMath/Source.cpp
+++ BadMath/Source.cpp
@@ -2,22 +2,22 @@
 #include <iomanip>
 using namespace std;
 
-float average(int i1, int i2, int i3)
+float average(float i1, float i2, float i3)
 {
-	return i1 + i2 + i3 / 3.0f;
+	return (i1 + i2 + i3 )/3;
 }
 
 int main()
 {
-	int n1 =0 , n2 = 0, n3 = 0;
+	float n1 =0 , n2 = 0, n3 = 0;
 	cout << "This program calculates the average of three numbers." << endl;
-	cout << "First number: "; cin >> n1;
-	cout << "Second number: "; cin >> n2;
-	cout << "Third number: "; cin >> n2;
+	cout << "Enter first number:  "; cin >> n1;
+	cout << "Enter second number: "; cin >> n2;
+	cout << "Enter third number:  "; cin >> n3;
 
 	float a = average(n1, n2, n3);
 
-	cout << setprecision(1) << fixed << "The average is " << a << endl;
+	cout << setprecision(2) << fixed << "The average is " << a << endl;
 
 	return 0;
 }
